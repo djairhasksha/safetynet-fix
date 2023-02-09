632990de6824b0ef2fcf50d48113856db99a87b7
djairhasksha <121382345+djairhasksha@users.noreply.github.com>
2023-02-09 12:05:38 +0200

    Update BuildHooks.kt

--- java/app/src/main/java/dev/kdrag0n/safetynetfix/BuildHooks.kt
+++ java/app/src/main/java/dev/kdrag0n/safetynetfix/BuildHooks.kt
@@ -4,10 +4,25 @@ import android.os.Build
 
 internal object BuildHooks {
     fun init() {
-        // Append a space to the device model name
         Build::class.java.getDeclaredField("MODEL").let { field ->
             field.isAccessible = true
-            field.set(null, Build.MODEL + " ")
+            field.set(null, "Pixel XL")
+        }
+        Build::class.java.getDeclaredField("PRODUCT").let { field ->
+            field.isAccessible = true
+            field.set(null, "marlin")
+        }
+        Build::class.java.getDeclaredField("DEVICE").let { field ->
+            field.isAccessible = true
+            field.set(null, "marlin")
+        }
+        Build::class.java.getDeclaredField("FINGERPRINT").let { field ->
+            field.isAccessible = true
+            field.set(null, "google/marlin/marlin:7.1.2/NJH47F/4146041:user/release-keys")
+        }
+        Build::class.java.getDeclaredField("DEVICE_INITIAL_SDK_INT").let { field ->
+            field.isAccessible = true
+            field.set(null, 25)
         }
     }
 }
